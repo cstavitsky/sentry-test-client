1b28c05aa53979343bead0efb590bdbaa1c43045
lemelinad <adam.lemelin@alsac.stjude.org>
2022-06-01 13:39:53 -0500

    Test changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link} from "react-router-dom";
+//import * as Sentry from "@sentry/react";
 
 function App() {
+  const getData = () => {
+    //const trx = Sentry.startTransaction({name: 'fetch'});
+    //Sentry.getCurrentHub().configureScope(scope => scope.setSpan(trx));
+
+    fetch("https://localhost:7095/WeatherForecast")
+      .then(res => res.json())
+      .then(json => console.log(json));
+      //.finally(() => trx.finish());
+  }
+
   return (
     <div className="App">
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Page 1</Link> |{" "}
+        <Link to="/expenses">Page 2</Link>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button" onClick={() => getData()}>Get data</button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,45 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import {BrowserTracing} from "@sentry/tracing";
+import {
+  Routes,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+  Route,
+} from "react-router-dom";
+
+Sentry.init({
+  dsn: "https://d05c5f62b27f4be5b13eea70657d1c30@o1252838.ingest.sentry.io/6419153",
+  integrations: [new BrowserTracing({
+    routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+      React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes
+    )
+  })],
+  debug: true,
+  tracesSampleRate: 1.0,
+});
+
+const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <SentryRoutes>
+        <Route path="/" element={<App />} />
+        <Route path="/invoices" element={<App/>}/>
+        <Route path="/expenses" element={<App/>}/>
+      </SentryRoutes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
